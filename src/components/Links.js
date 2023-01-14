690d399c52030cfb69a013bb194c9f8c836f3a54
ryan605 <langatryan26@gmail.com>
2023-01-14 14:07:03 +0300

    props

---
+++ src/components/Links.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Links(props){
+    console.log()
+    return(
+        <div>
+        <h3>Links</h3>
+<a href={props.github}>{props.github}</a>
+<a href={props.linkedin}>{props.linkedin}</a>
+        </div>
+    )
+}
+export default Links
